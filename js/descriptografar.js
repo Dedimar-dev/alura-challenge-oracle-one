fc5526519fcf9c312153ebe20aa1cc4eb6a6919f
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-01-18 19:08:48 -0300

    criacão da função de descriptografar

---
+++ js/descriptografar.js
@@ -0,0 +1,16 @@
+
+
+const descriptografar = (texto) => {
+  const arrayPalavras = texto.split(' ');
+
+  let novoTexto = arrayPalavras.map(palavra => {
+    palavra = palavra.replace(/ufat/g, 'u');
+    palavra =  palavra.replace(/enter/g, 'e');
+    palavra = palavra.replace(/imes/g, 'i');
+    palavra = palavra.replace(/ai/g, 'a');
+    palavra = palavra.replace(/ober/g, 'o');
+    palavra = palavra.replace(/ufat/g, 'u');
+    return palavra
+  })
+  return novoTexto.join(' ');
+}
