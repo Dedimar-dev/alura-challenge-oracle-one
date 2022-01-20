66e35cead7055ba7e6d52fab1c93290c9d348e29
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-01-20 17:44:37 -0300

    Mudança na lógica da Descriptografia

--- js/descriptografar.js
+++ js/descriptografar.js
@@ -2,12 +2,40 @@ const descriptografar = (texto) => {
   const arrayPalavras = texto.split(' ');
 
   let novoTexto = arrayPalavras.map(palavra => {
-    palavra =  palavra.replace(/enter/g, 'e');
-    palavra = palavra.replace(/imes/g, 'i');
-    palavra = palavra.replace(/ai/g, 'a');
-    palavra = palavra.replace(/ober/g, 'o');
-    palavra = palavra.replace(/ufat/g, 'u');
+    if (palavra.includes('enter')) {
+      palavra =  palavra.replace(/enter/g, 'e'); 
+      mostrarMensagemSucesso = true;
+    }
+
+    if (palavra.includes('imes')) {
+      palavra = palavra.replace(/imes/g, 'i');
+      mostrarMensagemSucesso = true;
+    }
+
+    if (palavra.includes('ai')) {
+      palavra = palavra.replace(/ai/g, 'a');
+      mostrarMensagemSucesso = true;
+    }
+
+    if (palavra.includes('ober')) {
+      palavra = palavra.replace(/ober/g, 'o');
+      mostrarMensagemSucesso = true;
+    }
+
+    if (palavra.includes('ufat')) {
+      palavra = palavra.replace(/ufat/g, 'u');
+      mostrarMensagemSucesso = true;
+    }
     return palavra
   })
-  return novoTexto.join(' ');
+  
+  novoTexto = novoTexto.join(' ');
+  
+  if (!mostrarMensagemSucesso) {
+    const mensagem1 = 'Mensagem inválida! ';
+    const mensagem2 = 'A descriptografia não pode ser realizada.'
+    mostrarMensagemErro(novoTexto, mensagem1, mensagem2);
+  }
+
+  return novoTexto;
 }
